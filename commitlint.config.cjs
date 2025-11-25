3fde01810913140e8ab88d776c130458d6cb97b3
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2025-11-25 08:22:31 +0700

    ci(commitlint): disable header-max-length & body-max-line-length

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0], // Disable the rule
+    'body-max-line-length': [0], // Disable the rule
+  },
+}
